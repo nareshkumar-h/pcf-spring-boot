18d7157e77e942ae64004c4faac308f565c728e1
Naresh Kumar H <nareshkumarh@live.com>
2019-05-08 11:40:51 -0500

    Intial commit

---
+++ src/main/java/com/naresh/pcfspringbootdemo/PcfSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.naresh.pcfspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfSpringbootDemoApplication.class, args);
+	}
+
+}
